2005ec3ff5c836b066109d09257082bf8d6f8ba2
trimstray <contact@nslab.at>
2018-01-21 14:43:57 +0100

    updated setup.sh - signed-off-by: trimstray <contact@nslab.at>

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+printf "%s" "Create symbolic link to /usr/local/bin: "
+
+if [[ -d "/usr/local/bin" ]] ; then
+
+  ln -s bin/git-template-full /usr/local/bin
+  printf "%s\n" "done."
+
+else
+
+  printf "%s\n" "fail."
+
+fi
+
+printf "%s" "Create man page to /usr/local/man/man8: "
+
+if [[ -d "/usr/local/man/man8" ]] ; then
+
+  cp doc/man8/git-template-full /usr/local/man/man8
+  gzip /usr/local/man/man8/git-template-full
+
+  printf "%s\n" "done."
+
+else
+
+  printf "%s\n" "fail."
+
+fi
