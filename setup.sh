cdef4db60d7b23f7e16012bf5a471deb414babcf
trimstray <contact@nslab.at>
2018-01-21 15:16:41 +0100

    added 'install' and 'uninstall' options - signed-off-by: trimstray <contact@nslab.at>

--- setup.sh
+++ setup.sh
@@ -1,29 +1,53 @@
 #!/usr/bin/env bash
 
+_arg="$1"
+
 # To adapt this script to your project, do the following:
 #   - vim: ESC:%s/git-template-full/your-project/g
 #   - sed: set -i 's/git-template-full/your-project/g'
 
-printf "%s\n" "Create symbolic link to /usr/local/bin"
+if [[ "$1" == "install" ]] ; then
 
-if [[ -e "bin/git-template-full" ]] ; then
+  printf "%s\n" "Create symbolic link to /usr/local/bin"
 
-  if [[ ! -e "/usr/local/bin/git-template-full" ]] ; then
+  if [[ -e "bin/git-template-full" ]] ; then
 
-    ln -s bin/git-template-full /usr/local/bin
+    if [[ ! -e "/usr/local/bin/git-template-full" ]] ; then
+
+      ln -s bin/git-template-full /usr/local/bin
+
+    fi
 
   fi
 
-fi
+  printf "%s\n" "Create man page to /usr/local/man/man8"
 
-printf "%s\n" "Create man page to /usr/local/man/man8"
+  if [[ -e "doc/man8/git-template-full.8" ]] ; then
 
-if [[ -e "doc/man8/git-template-full.8" ]] ; then
+    if [[ ! -e "/usr/local/man/man8/git-template-full.8.gz" ]] ; then
 
-  if [[ ! -e "/usr/local/man/man8/git-template-full.gz" ]] ; then
+      cp doc/man8/git-template-full.8 /usr/local/man/man8
+      gzip /usr/local/man/man8/git-template-full.8
 
-    cp doc/man8/git-template-full.8 /usr/local/man/man8
-    gzip /usr/local/man/man8/git-template-full.8
+    fi
+
+  fi
+
+elif [[ "$1" == "uninstall" ]] ; then
+
+  printf "%s\n" "Remove symbolic link from /usr/local/bin"
+
+  if [[ -e "/usr/local/bin/git-template-full" ]] ; then
+
+    rm "/usr/local/bin/git-template-full"
+
+  fi
+
+  printf "%s\n" "Remove man page from /usr/local/man/man8"
+
+  if [[ -e "/usr/local/man/man8/git-template-full.8.gz" ]] ; then
+
+    rm "/usr/local/man/man8/git-template-full.8.gz"
 
   fi
 
