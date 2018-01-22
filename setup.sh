b4ae235ed6e938ed90f2d3c296a49a7e23ec8344
trimstray <contact@nslab.at>
2018-01-22 23:02:54 +0100

    setup.sh: minor fixes - signed-off-by: trimstray <contact@nslab.at>

--- setup.sh
+++ setup.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
 # To adapt this script to your project, do the following:
-#   - vim: ESC:%s/multitor/your-project/g
-#   - sed: set -i 's/multitor/your-project/g'
+#   - vim: ESC:%s/git-template-full/your-project/g
+#   - sed: set -i 's/git-template-full/your-project/g'
 
+readonly _dir="$(dirname "$(readlink -f "$0")")"
+
+# shellcheck disable=SC2034
 _arg="$1"
 
 if [[ "$1" == "install" ]] ; then
 
   printf "%s\n" "Create symbolic link to /usr/local/bin"
 
-  if [[ -e "bin/git-template-full" ]] ; then
+  if [[ -e "${_dir}/bin/git-template-full" ]] ; then
 
     if [[ ! -e "/usr/local/bin/git-template-full" ]] ; then
 
-      ln -s bin/git-template-full /usr/local/bin
+      ln -s "${_dir}/bin/git-template-full" /usr/local/bin
 
     fi
 
@@ -22,11 +25,11 @@ if [[ "$1" == "install" ]] ; then
 
   printf "%s\n" "Create man page to /usr/local/man/man8"
 
-  if [[ -e "doc/man8/git-template-full.8" ]] ; then
+  if [[ -e "${_dir}/doc/man8/git-template-full.8" ]] ; then
 
     if [[ ! -e "/usr/local/man/man8/git-template-full.8.gz" ]] ; then
 
-      cp doc/man8/git-template-full.8 /usr/local/man/man8
+      cp "${_dir}/doc/man8/git-template-full.8" /usr/local/man/man8
       gzip /usr/local/man/man8/git-template-full.8
 
     fi
@@ -54,4 +57,3 @@ elif [[ "$1" == "uninstall" ]] ; then
 fi
 
 exit 0
-
